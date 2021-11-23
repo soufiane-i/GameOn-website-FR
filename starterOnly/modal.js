4f608d0dc78c5a09b67842ad53a0f0cfcfbb4c84
Soufiane Irssal <soufiane.irssal@gmail.com>
2021-11-23 17:07:12 +0100

    fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,23 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCrossBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// quit modal event
+modalCrossBtn.forEach((btn) => btn.addEventListener("click", quitModal));
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// quit modal form
+function quitModal() {
+  modalbg.style.display = "none";
 }
 
 

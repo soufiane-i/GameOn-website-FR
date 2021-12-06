daad7dd50fd55f668ae54a0da7811c12bf90edfd
Soufiane Irssal <soufiane.irssal@gmail.com>
2021-12-06 15:34:47 +0100

    message quand envoie reussi

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,7 +13,7 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const submitBtn = document.querySelectorAll(".btn-submit");
 const modalCrossBtn = document.querySelectorAll(".close");
 const textControl = document.querySelectorAll(".text-control");
-
+const form = document.getElementsByName("reserve");
 const prenom = document.getElementById("first");
 const nom = document.getElementById("last");
 const email = document.getElementById("email");
@@ -23,7 +23,7 @@ const conditionsTerms = document.getElementById("checkbox1");
 const formData = document.querySelectorAll(".formData");
 const locations = document.getElementsByName("location");
 const modalConfirmation = document.querySelector(".modalConfirmation");
-
+const thanksCloseBtn = document.querySelectorAll(".modalConfirmation-btn-close");
 
 
 
@@ -38,6 +38,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // quit modal event
 modalCrossBtn.forEach((btn) => btn.addEventListener("click", quitModal));
 
+thanksCloseBtn.forEach((btn) => btn.addEventListener("click", quitModal));
+
+
+
 // launch modal form function
 function launchModal() {
   modalbg.style.display = "block";
@@ -46,7 +50,9 @@ function launchModal() {
 // quit modal form function
 function quitModal() {
   modalbg.style.display = "none";
+  modalConfirmation.style.display = "none";
 }
+
 
 
        
@@ -65,29 +71,33 @@ let quantityRegex = /^\d{1,2}$/;
 
 
 // submit event 
-submitBtn.forEach((btn) => btn.addEventListener("click", submitFunct)); 
+submitBtn.forEach((btn) => btn.addEventListener("click", submitFunct));
+
+
 
 
 //submit function
 function submitFunct(e){
 
+
+
  /*if input is : 
       - valid = don't display error message + check next input 
       - invalid = don't submit + don't display its error message
       - valid and it's the last = submit  */
-   if(prenomRegex.test(prenom.value) == true){
+   if(prenomRegex.test(prenom.value.trim()) == true){
     formData[0].dataset.errorVisible = "false";
 
-    if (nomRegex.test(nom.value) == true){
+    if (nomRegex.test(nom.value.trim()) == true){
       formData[1].dataset.errorVisible = "false";
 
-      if(emailRegex.test(email.value)){
+      if(emailRegex.test(email.value.trim())){
         formData[2].dataset.errorVisible = "false";
 
       if(date.value){
         formData[3].dataset.errorVisible = "false";
 
-        if(quantityRegex.test(quantity.value)){
+        if(quantityRegex.test(quantity.value.trim())){
           formData[4].dataset.errorVisible = "false";
 
           if(locations[0].checked == true || locations[1].checked == true || locations[2].checked == true || locations[3].checked == true || locations[4].checked == true || locations[5].checked == true) {
@@ -95,7 +105,8 @@ function submitFunct(e){
 
             if(conditionsTerms.checked) {
               //submit
-              console.log("submit");
+              submitClear();
+              e.preventDefault();
             } else {
               console.log("term pas check");
               formData[6].dataset.errorVisible = "true";
@@ -106,7 +117,6 @@ function submitFunct(e){
               console.log("location pas bon");
               formData[5].dataset.errorVisible = "true";
               e.preventDefault();
-              
             }
         
           } else {
@@ -144,8 +154,18 @@ function submitFunct(e){
 //------------------------------------------------------------------------------------------
 
 
-
-
+//
+function submitClear(){
+  formData[6].dataset.errorVisible = "false";
+  modalConfirmation.style.display = "block";
+  for(let i = 0; i<textControl.length; i++){
+    textControl[i].value = "";
+  }
+  for(let i = 0; i<locations.length; i++){
+    locations[i].checked = false;
+  }
+  conditionsTerms.checked = false
+}
 
                
 
@@ -154,3 +174,4 @@ function submitFunct(e){
 
 
 
+console.log(prenom.value);

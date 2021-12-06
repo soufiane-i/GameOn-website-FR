5ffc3df1fb4518a70b95eb535679c1b7c3a59149
Soufiane Irssal <soufiane.irssal@gmail.com>
2021-12-06 11:47:20 +0100

    todo #2 implémenter formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,6 +20,16 @@ const email = document.getElementById("email");
 const date = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 const city = document.getElementById("location1");
+const conditionsTerms = document.getElementById("checkbox1");
+const formData = document.querySelectorAll(".formData");
+const locations = document.getElementsByName("location");
+const modalConfirmation = document.querySelector(".modalConfirmation");
+
+
+
+
+
+
  
 
 
@@ -29,55 +39,183 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // quit modal event
 modalCrossBtn.forEach((btn) => btn.addEventListener("click", quitModal));
 
-// launch modal form
+// launch modal form function
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// quit modal form
+// quit modal form function
 function quitModal() {
   modalbg.style.display = "none";
 }
 
 
        
-// Check Form
+// Form check ----------------------------------------------------------------
 
-for (let i=0; i<textControl.length; i++ ){
-  textControl[i].setAttribute("required", "");
-}
-
-city.checked = true;
-
-console.log(textControl.length);
 //REGEX
 
-let prenomRegex = /^[a-z ,.'-]{2,}$/i;
+let prenomRegex = /^[a-z-]{2,}$/i;
 let nomRegex = /^[a-z ,.'-]{2,}$/i;
-let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-let dateRegex = /^[a-z ,.'-]{2,}$/i;
+let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+let dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
 let quantityRegex = /^\d{1,2}$/;
 
-
-
-submitBtn.forEach((btn) => btn.addEventListener("click", submitFunct));
-
-
-function submitFunct(e){
-   // empecher la page de se recharger
-  if(prenom.value.match(prenomRegex) 
-  && nom.value.match(nomRegex) 
-  && email.value.match(emailRegex)
-  && quantity.value.match(quantityRegex)){
-    console.log("kokok");
-  } else {
-    e.preventDefault();
-    console.log("non");
-  }    
+let regexs = {
+  prenomRegex : /^[a-z-]{2,}$/i,
+  nomRegex : /^[a-z ,.'-]{2,}$/i,
+  emailRegex : /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+  date: date,
 
 }
 
 
+
+// submit event 
+submitBtn.forEach((btn) => btn.addEventListener("click", submitFunct)); 
+
+// Custom error message
+/* prenom.reportValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+nom.reportValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+conditionsTerms.reportValidity("Vous devez vérifier que vous acceptez les termes et conditions.");
+date.reportValidity("Vous devez entrer votre date de naissance."); */
+
+//submit function
+function submitFunct(e){
+
+  
+   if(prenomRegex.test(prenom.value) == true){
+    
+
+    if (nomRegex.test(nom.value) == true){
+      
+
+      if(emailRegex.test(email.value)){
+      
+
+      if(date.value){
+        
+
+        if(quantityRegex.test(quantity.value)){
+         
+
+          if(locations[0].checked == true || locations[1].checked == true || locations[2].checked == true || locations[3].checked == true || locations[4].checked == true || locations[5].checked == true) {
+            
+
+            if(conditionsTerms.checked) {
+              //submit
+              console.log("submit");
+            } else {
+              console.log("term pas check")
+              e.preventDefault();
+            }
+          
+            } else {
+              console.log("location pas bon");
+              e.preventDefault();
+              
+            }
+        
+          } else {
+            console.log("quantity pas bon");
+            e.preventDefault();
+          }
+      
+        } else {
+          console.log("date pas bon");
+          e.preventDefault();
+        }
+    
+      } else {
+        console.log("email pas bon");
+        e.preventDefault();
+      }
+      
+    }else {
+      console.log("nom faux");
+      e.preventDefault();
+    } 
+  } else {
+    console.log("prenom faux");
+    e.preventDefault();
+  }  
+  
+}
+
+// set required attribute to all text control
+
+
+//check one of the radio button. Then one radio button always is check  
+
+
+
+function prenomCheck(){
+  if(prenomRegex.test(prenom.value)){
+    
+    return true;
+} else { return false;
+  
+  
+}
+}
+
+function nomCheck(){
+  if(nomRegex.test(nom.value)){
+    
+    
+  } else {
+  
+  }
+}
+
+function emailCheck(){
+  if(emailRegex.test(email.value)){
+    
+    
+  } else {
+  
+  }
+}
+
+function dateCheck(){
+  if(dateRegex.test(date.value)){
+    
+    
+  } else {
+  
+  }
+}
+
+function quantityCheck(){
+  if(quantityRegex.test(quantity.value)){
+    
+    
+  } else {
+  
+  
+  }
+}
+
+function locationsCheck(){
+  if (locations[0].checked == false && locations[1].checked == false && locations[2].checked == false && locations[3].checked == false && locations[4].checked == false && locations[5].checked == false) {
+    
+  } else  {
+  
+  }
+}
+
+function conditionTermsCheck(){
+  if (conditionsTerms.checked)
+  {
+   
+  } else {
+    
+  }
+}
+
+
+
+//------------------------------------------------------------------------------------------
 
 
 
